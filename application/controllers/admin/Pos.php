c4a9ad3ca52d0705b7d22f0045be644ca227d30c
nawawi932@gmail.com <nawawi932@gmail.com>
2017-01-15 20:42:51 +0700

    add page pos_new

--- application/controllers/admin/Pos.php
+++ application/controllers/admin/Pos.php
@@ -3,14 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pos extends CI_Controller {
 
-	public function index()
-	{
+	public function index(){
 		$this->load->view('assets/csslinkadm');
 		$this->load->view('admin/navbar');
 		$this->load->view('admin/pos');
 		$this->load->view('assets/jslinkadm');
 	}
 
+	public function newpos(){
+		$this->load->view('assets/csslinkadm');
+		$this->load->view('admin/navbar');
+		$this->load->view('admin/pos_new');
+		$this->load->view('assets/jslinkadm');
+	}
 }
 
 /* End of file pos.php */
