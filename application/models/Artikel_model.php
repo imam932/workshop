693fc7a93c270da4184a783e1b4462e91bbfd973
nawawi932@gmail.com <nawawi932@gmail.com>
2017-01-16 13:03:51 +0700

    summernote

---
+++ application/models/Artikel_model.php
@@ -0,0 +1,64 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Artikel_model extends CI_Model
+{
+	$table = "artikel";
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function select_all()
+	{
+		$q = $this->db->get($table);
+
+		if($q->num_rows() > 0)
+		{
+			return $q->result();
+		}
+		else
+		{
+			return array();
+		}
+	}
+
+	public function select_by_id($id)
+	{
+		$this->db->where('id_artikel', $id);
+
+		$q = $this->db->get($table);
+
+		if ($q->num_rows() == 1)
+		{
+			return $q->row();
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public function insert($data)
+	{
+		$this->db->insert($table, $data);
+	}
+
+	public function update($data, $id)
+	{
+		$this->db->where('id_artikel', $id);
+		$this->db->update($table, $data);
+	}
+
+	public function delete($id)
+	{
+		$this->db->where('id', $id);
+		$this->db->delete($table);
+	}
+}
+
+/* End of file Artikel_model.php */
+/* Location: ./application/models/Artikel_model.php */
+
+?>
---
+++ application/models/Autentikasi_model.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Autentikasi_model extends CI_Model
+{
+
+  $table = "autentikasi";
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function select_all()
+  {
+    $query = $this->db->get($table);
+
+    if($query->num_rows() > 0)
+    {
+      return $query->result();
+    }
+    else
+    {
+      return array();
+    }
+  }
+
+  public function select_by_id($id)
+  {
+    $this->db->where('id_user', $id);
+    $query = $this->db->get($table);
+
+    if($query->num_rows() == 1)
+    {
+      return $query->result();
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  public function insert($data)
+  {
+    $this->db->insert($table, $data);
+  }
+
+  public function update($data, $id)
+  {
+    $this->db->where('id_user', $id);
+    $this->db->update($table, $data);
+  }
+
+  public function delete($id)
+  {
+    $this->db->where('id_user', $id);
+    $this->db->delete($table);
+  }
+}
