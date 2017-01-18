927839e0271137c92d9920cca2c5f46df8827ed4
mnindra <mnindrazaka@gmail.com>
2017-01-18 16:19:26 +0700

    fix dataTable on user page

--- application/views/admin/user.php
+++ application/views/admin/user.php
@@ -6,36 +6,38 @@
 		</p> <br>
 
 		<div class="row">
-			<table id="example1" class="table table-striped table-bordered" cellspacing="0" width="100%">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Genre</th>
-						<th>Birth</th>
-						<th>address</th>
-						<th>Phone</th>
-						<th>Action</th>
-					</tr>
-				</thead>
+			<div class="col-lg-12">
+				<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Genre</th>
+							<th>Birth</th>
+							<th>address</th>
+							<th>Phone</th>
+							<th>Action</th>
+						</tr>
+					</thead>
 
-				<tbody>
-					<tr>
-						<td>sdlkfgdfl</td>
-						<td>sdhsd</td>
-						<td>sdsd</td>
-						<td>sdsldlskdhslkd la kshad slkd hsdls sldkh</td>
-						<td>sbdd</td>
-						<td>
-							<a href="barang_edit.php?id=<?php  ?>" class="btn btn-primary btn-sm">
-								edit
-							</a>
-							<a href="barang_delete.php?id=<?php  ?>" class="btn btn-danger btn-sm">
-								delete
-							</a>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+					<tbody>
+						<tr>
+							<td>sdlkfgdfl</td>
+							<td>sdhsd</td>
+							<td>sdsd</td>
+							<td>sdsldlskdhslkd la kshad slkd hsdls sldkh</td>
+							<td>sbdd</td>
+							<td>
+								<a href="barang_edit.php?id=<?php  ?>" class="btn btn-primary btn-sm">
+									edit
+								</a>
+								<a href="barang_delete.php?id=<?php  ?>" class="btn btn-danger btn-sm">
+									delete
+								</a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
 </div>
