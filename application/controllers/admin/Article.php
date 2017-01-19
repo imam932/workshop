c7af1b63617b2d180eccca7475446e2b5fd699e3
mnindra <mnindrazaka@gmail.com>
2017-01-19 19:43:13 +0700

    form validation article

--- application/controllers/admin/Article.php
+++ application/controllers/admin/Article.php
@@ -14,6 +14,12 @@ class Article extends Admin_Controller {
 	{
 		//load data
 		$data['article'] = $this->Model_article->select_all();
+		// error handling
+		if(!empty($this->session->flashdata('message')))
+		{
+			$data['message'] = $this->session->flashdata('message');
+		}
+		// load page
 		$data['content'] = $this->load->view('admin/article', $data, TRUE);
 
 		//load template
@@ -28,20 +34,38 @@ class Article extends Admin_Controller {
 	{
 		if ($this->input->server('REQUEST_METHOD') == 'POST')
 		{
-			$data['id_article'] = random_string('alnum', 6) . date('my') . random_string('alnum', 5);
-			$data['date'] = date('Y-m-d h:i:s');
-			$data['title'] = $this->input->post('title');
-			$data['id_category'] = $this->input->post('id_category');
-			$data['posting'] = $this->input->post('posting');
-			$data['id_user'] = $this->session->userdata('logged_in')['id_user'];
+			//form validation
+	    $this->form_validation->set_rules('title', 'Title', 'trim|required|xss_clean');
+	    $this->form_validation->set_rules('id_category', 'Category', 'required');
 
-			$this->Model_article->insert($data);
+	    if(!$this->form_validation->run())
+	    {
+	      $this->session->set_flashdata('error', validation_errors());
+				redirect('admin/Article/New', 'refresh');
+	    }
+			else
+			{
+				$data['id_article'] = random_string('alnum', 6) . date('my') . random_string('alnum', 5);
+				$data['date'] = date('Y-m-d h:i:s');
+				$data['title'] = $this->input->post('title');
+				$data['id_category'] = $this->input->post('id_category');
+				$data['posting'] = $this->input->post('posting');
+				$data['id_user'] = $this->session->userdata('logged_in')['id_user'];
+				$this->Model_article->insert($data);
 
-			redirect('admin/Article', 'refresh');
+				$this->session->set_flashdata('message', 'Success ! Article has been created');
+				redirect('admin/Article', 'refresh');
+			}
 		}
 
 		// load data
 		$data['category'] = $this->Model_category->select_all();
+		// error handling
+		if(!empty($this->session->flashdata('error')))
+		{
+			$data['error'] = $this->session->flashdata('error');
+		}
+		// load page
 		$data['content'] = $this->load->view('admin/article_new', $data, TRUE);
 
 		//load template
@@ -55,7 +79,7 @@ class Article extends Admin_Controller {
 	function deleteArticle($id)
 	{
 		$this->Model_article->delete($id);
-
+		$this->session->set_flashdata('message', 'Success ! Article has been deleted');
 		redirect('admin/Article', 'refresh');
 	}
 
@@ -63,21 +87,39 @@ class Article extends Admin_Controller {
 	{
 		if ($this->input->server('REQUEST_METHOD') == 'POST')
 		{
-			$data['title'] = $this->input->post('title');
-			$data['id_category'] = $this->input->post('id_category');
-			$data['posting'] = $this->input->post('posting');
-			$data['id_user'] = $this->session->userdata('logged_in')['id_user'];
-			$this->Model_article->update($data, $id);
+			//form validation
+	    $this->form_validation->set_rules('title', 'Title', 'trim|required|xss_clean');
 
-			redirect('admin/Article', 'refresh');
+	    if(!$this->form_validation->run())
+	    {
+	      $this->session->set_flashdata('error', validation_errors());
+				redirect('admin/Article/editArticle/' . $id, 'refresh');
+	    }
+			else
+			{
+				$data['title'] = $this->input->post('title');
+				$data['id_category'] = $this->input->post('id_category');
+				$data['posting'] = $this->input->post('posting');
+				$data['id_user'] = $this->session->userdata('logged_in')['id_user'];
+				$this->Model_article->update($data, $id);
+
+				$this->session->set_flashdata('message', 'Success ! Article has been edited');
+				redirect('admin/Article', 'refresh');
+			}
 		}
 
 		// load data
 		$data['category'] = $this->Model_category->select_all();
 		$data['article'] = $this->Model_article->select_by_id($id);
+		// error handling
+		if(!empty($this->session->flashdata('error')))
+		{
+			$data['error'] = $this->session->flashdata('error');
+		}
+		// load page
 		$data['content'] = $this->load->view('admin/article_edit', $data, TRUE);
 
-		//load template
+		// load template
 		$data['title'] = "Article";
 		$data['desc'] = "Edit Article";
 		$data['breadcrumb'] = array('Dashboard', 'Article', 'Edit');
