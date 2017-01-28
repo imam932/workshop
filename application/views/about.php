1d63cba5c1b8fa8243145661b2a4b55015c3b7c8
nawawi932@gmail.com <nawawi932@gmail.com>
2017-01-28 15:52:51 +0700

    about layout

--- application/views/about.php
+++ application/views/about.php
@@ -1,19 +1,74 @@
 <div class="container">
   <div class="row about col-lg-6 col-sm-12">
-    <div class="title">
+    <div class="about__title">
       <h2>Foto Semua Angkatan</h2>
     </div>
     <img src="<?= base_url() ?>assets/img/20161217_121043.jpg" alt="Foto angkatan">
   </div>
   <div class="row about col-lg-6 col-sm-12">
-    <div class="title">
+    <div class="about__title">
+      <h2>Sekilas Tentang WRI</h2>
+      <div class="about_content">
+        <b>Workshop Riset Informatika</b> adalah komunitas yang memiliki tujuan
+        bersama untuk membina dan mengembangkan kreatifitas mahasiswa
+        dalam bidang ilmu pengetahuan dan teknologi.
+      </div>
+    </div>
+
+  </div>
+  <div class="row about col-lg-12 col-md-12">
+    <hr class="tall">
+    <div class="about__title">
       <h2>Struktur Organisasi</h2>
+    </div>
+
+    <!-- 1 -->
+    <div class="organisasi col-lg-12 col-md-12">
+      <anggota>
+        <h4>Hilal</h4>
+        <i><p>Ketua umum</p></i>
+        <img src="<?= base_url() ?>assets/img/avatar-2.jpg" alt="" class="img-circle">
+      </anggota>
+    </div>
+
+    <!-- 2 -->
+    <div class="organisasi col-lg-4 col-md-12">
+      <anggota>
+        <h4>Hilal</h4>
+        <i><p>Ketua divisi</p></i>
+        <img src="<?= base_url() ?>assets/img/avatar-2.jpg" alt="" class="img-circle">
+      </anggota>
+    </div>
+    <div class="organisasi col-lg-4 col-md-12">
+      <anggota>
+        <h4>Hilal</h4>
+        <i><p>Ketua divisi</p></i>
+        <img src="<?= base_url() ?>assets/img/avatar-2.jpg" alt="" class="img-circle">
+      </anggota>
+    </div>
+    <div class="organisasi col-lg-4 col-md-12">
+      <anggota>
+        <h4>Hilal</h4>
+        <i><p>Ketua divisi</p></i>
+        <img src="<?= base_url() ?>assets/img/avatar-2.jpg" alt="" class="img-circle">
+      </anggota>
+    </div>
+
+    <!-- 3 -->
+    <div class="organisasi col-lg-6 col-md-12">
+      <anggota>
+        <h4>Hilal</h4>
+        <i><p>Sekretaris</p></i>
+        <img src="<?= base_url() ?>assets/img/avatar-2.jpg" alt="" class="img-circle">
+      </anggota>
     </div>
     <div class="organisasi col-lg-6 col-md-12">
       <anggota>
         <h4>Hilal</h4>
-      <img src="<?= base_url() ?>assets/img/avatar-2.jpg" alt="" class="img-circle">
-    </anggota>
+        <i><p>Bendahara</p></i>
+        <img src="<?= base_url() ?>assets/img/avatar-2.jpg" alt="" class="img-circle">
+      </anggota>
     </div>
+
   </div>
 </div>
